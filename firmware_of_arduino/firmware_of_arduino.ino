565207b396618cba18b1b820e7d9e1ddbe2d4827
Mironkin Petr <mirok@infsys.ru>
2024-09-19 01:14:29 +0300

    Доделки и тщетные попытки получить данные с датчика.

--- firmware_of_arduino/firmware_of_arduino.ino
+++ firmware_of_arduino/firmware_of_arduino.ino
@@ -1,5 +1,6 @@
 // адрес датчика для D2 Sensor 1 : 0x28, 0xFF, 0x64, 0x1E, 0x83, 0x61, 0xCB, 0x99
-
+#include <Arduino.h>
+#include "ESP8266WiFi.h"
 #include <ESP8266WebServer.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
@@ -15,38 +16,65 @@ DallasTemperature sensors(&oneWire);
 
 float tempSensor1, tempSensor2, tempSensor3;
 
-uint8_t sensor1[8] = { 0x28, 0xFF, 0x64, 0x1E, 0x83, 0x61, 0xCB, 0x99  };
+#define TEMPERATURE_PRECISION 12  // точность измерений (9 ... 12)
+uint8_t sensor1[8] = { 0x28, 0xFF, 0x64, 0x1E, 0x83, 0x61, 0xCB, 0x99 };
 
 
 /* Введите SSID и пароль от вашей сети*/
-const char* ssid = "Xiaomi_49C2";  // SSID 
-const char* password = "Killme21";  //пароль
+//const char* ssid = "Xiaomi_49C2";  // SSID
+//const char* password = "Killme21";  //пароль
 
-ESP8266WebServer server(80);             
- 
-void setup() 
-{
-  Serial.begin(115200);
+const char* ssid = "RT-GPON-96C8";         // SSID
+const char* password = "718281828459045";  //пароль
+
+//const char* ssid = "termoduino";           // SSID
+//const char* password = "718281828459045";  //пароль
+
+const WiFiMode wiFiMode = WIFI_STA;  // WIFI_AP - access point   WIFI_STA - station
+
+DeviceAddress Thermometer;
+
+ESP8266WebServer server(80);
+
+void setup() {
+  Serial.begin(9600);
   delay(100);
-  
-  sensors.begin();              
 
-  Serial.println("Connecting to ");
-  Serial.println(ssid);
+  sensors.begin();                                            // инициализация DS18B20
+  sensors.getAddress(Thermometer, 0);                         // адрес DS18B20 (поиск по индексу)
+  sensors.setResolution(Thermometer, TEMPERATURE_PRECISION);  // установка точности измерения 9...12 разрядов
+  Serial.print("Sensor addr: ");
+  Serial.println((int)&Thermometer);
 
-  // подключиться к вашей локальной wi-fi сети
-  WiFi.begin(ssid, password);
 
-  // проверить, выполнено ли подключение wi-fi сети
-  while (WiFi.status() != WL_CONNECTED) 
-  {
-    delay(1000);
-    Serial.print(".");
+  if (wiFiMode == WIFI_AP) {
+    WiFi.mode(WIFI_AP);
+    // Точка доступа
+    if (!WiFi.softAP(ssid, password)) {
+      Serial.println("Soft AP creation failed.");
+      while (1)
+        ;
+    }
+    IPAddress myIP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(myIP);
+  } else {
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    // подключиться к вашей локальной wi-fi сети
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+
+    // проверить, выполнено ли подключение wi-fi сети
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(1000);
+      Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected..!");
+    Serial.print("Got IP: ");
+    Serial.println(WiFi.localIP());
   }
-  Serial.println("");
-  Serial.println("WiFi connected..!");
-  Serial.print("Got IP: ");  Serial.println(WiFi.localIP());
-
   server.on("/", handle_OnConnect);
   server.onNotFound(handle_NotFound);
 
@@ -54,41 +82,39 @@ void setup()
   Serial.println("HTTP server started");
 }
 
-void loop() 
-{
+void loop() {
   server.handleClient();
 }
 
-void handle_OnConnect() 
-{
+void handle_OnConnect() {
   sensors.requestTemperatures();
-  tempSensor1 = sensors.getTempC(sensor1); // Получить значение температуры
-  server.send(200, "text/html", SendHTML(tempSensor1,tempSensor2,tempSensor3)); 
+  tempSensor1 = sensors.getTempC(Thermometer);  // Получить значение температуры
+  Serial.print("tempSensor1 = ");
+  Serial.println(tempSensor1);
+  server.send(200, "text/html", SendHTML(tempSensor1, tempSensor2, tempSensor3));
 }
 
-void handle_NotFound()
-{
+void handle_NotFound() {
   server.send(404, "text/plain", "Not found");
 }
 
-String SendHTML(float tempSensor1,float tempSensor2,float tempSensor3)
-{
+String SendHTML(float tempSensor1, float tempSensor2, float tempSensor3) {
   String ptr = "<!DOCTYPE html> <html>\n";
-  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr +="<title>ESP8266 Temperature Monitor</title>\n";
-  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
-  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;}\n";
-  ptr +="p {font-size: 24px;color: #444444;margin-bottom: 10px;}\n";
-  ptr +="</style>\n";
-  ptr +="</head>\n";
-  ptr +="<body>\n";
-  ptr +="<div id=\"webpage\">\n";
-  ptr +="<h1>ESP8266 Temperature Monitor</h1>\n";
-  ptr +="<p>Living Room: ";
-  ptr +=tempSensor1;
-  ptr +="&deg;C</p>";
-  ptr +="</div>\n";
-  ptr +="</body>\n";
-  ptr +="</html>\n";
+  ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr += "<title>ESP8266 Temperature Monitor</title>\n";
+  ptr += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;}\n";
+  ptr += "p {font-size: 24px;color: #444444;margin-bottom: 10px;}\n";
+  ptr += "</style>\n";
+  ptr += "</head>\n";
+  ptr += "<body>\n";
+  ptr += "<div id=\"webpage\">\n";
+  ptr += "<h1>ESP8266 Temperature Monitor</h1>\n";
+  ptr += "<p>Living Room: ";
+  ptr += tempSensor1;
+  ptr += "&deg;C</p>";
+  ptr += "</div>\n";
+  ptr += "</body>\n";
+  ptr += "</html>\n";
   return ptr;
 }
